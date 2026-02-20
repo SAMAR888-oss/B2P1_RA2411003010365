289a45b7cdf8b8d4ad232450a8b17f37780e563c
SAMAR888-oss <your_gunchaara@gmail.com>
2026-02-20 11:23:37 +0530

    UC1: Application entry and welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application");
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome App     ");
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully.");
     }
 }
