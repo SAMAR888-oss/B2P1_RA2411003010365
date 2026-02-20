1dd57aef344c5e1da0265f0583ee859be7b5bcee
SAMAR888-oss <your_gunchaara@gmail.com>
2026-02-20 10:37:34 +0530

    UC1: Application entry and welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,5 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("===== UC6: Queue + Stack Based Palindrome =====");
-        System.out.print("Enter a word: ");
-        String input = scanner.nextLine();
-
-        // Convert to lowercase (optional but good)
-        input = input.toLowerCase();
-
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
-
-        // Add characters to both
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-            queue.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        // Compare elements
-        while (!stack.isEmpty()) {
-            if (stack.pop() != queue.remove()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
-            System.out.println("It is a Palindrome.");
-        } else {
-            System.out.println("It is NOT a Palindrome.");
-        }
-
-        scanner.close();
+        System.out.println("Welcome to Palindrome Application");
     }
 }
